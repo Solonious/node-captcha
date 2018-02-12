6232a1230667b10d859784e9bbb668c50ed5e42e
Sergii Solonar <sergeysolonar@gmail.com>
2018-02-12 22:47:08 +0200

    update code

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ app.use(app.router);
 
 app.get('/', routes.index);
 app.get('/captcha', routes.captcha);
+app.post('/captcha', routes.index);
+
 app.get('/users', users.list);
+
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var svgCaptcha = require('svg-captcha');
-
+var captchaText = '';
 
 /* GET home page. */
 exports.index = function(req, res){
@@ -8,8 +8,18 @@ exports.index = function(req, res){
 
 exports.captcha = function (req, res) {
     var captcha = svgCaptcha.create();
+    captchaText = captcha.text;
     req.session.captcha = captcha.text;
 
     res.type('svg');
     res.status(200).send(captcha.data);
+};
+
+exports.captchaCheck = function(req, res, err) {
+	if (err) res.send(err);
+
+	if (req.body.capData === captchaText) {
+		res.render('index', { title: 'Express' });
+	}
+
 };
